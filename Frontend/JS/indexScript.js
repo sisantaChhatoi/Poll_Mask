2cace63074fa0f776b238f57b96f8a7adc257276
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2025-02-04 13:32:14 +0530

    index webpage completed (pollingSlogans.txt added)

--- Frontend/JS/indexScript.js
+++ Frontend/JS/indexScript.js
@@ -1,29 +1,30 @@
 {
+  // The below fetches the data from pollingSlogans.txt and picks a random line from that to display on the website
 
-    //This generates random text in span of the HTML
+  async function loadSlogan() {
+    try {
+      const changeElement = document.getElementById("name");
 
-  const changeElement = document.getElementById("name");
+      const response = await fetch("/Frontend/JS/TXT/pollingSlogans.txt");
 
-  const diffNames = [
-    "Poll Mask!",
-    "the BEST Polling website!",
-    "our website!",
-    "the only website that respects privacy!",
-  ];
+      const slogans = await response.text();
 
-  setInterval(() => {
+      let diffNames = slogans.split("\n");
 
-    const index = Math.floor(Math.random() * (diffNames.length));
-    changeElement.innerHTML = diffNames[index];
+      setInterval(() => {
+        const index = Math.floor(Math.random() * diffNames.length);
+        changeElement.innerHTML = diffNames[index];
+      }, 3000);
+    } catch (error) {
+      console.error("something went wrong!");
+    }
+  }
 
-  }, 3000);
+  window.onload = loadSlogan;
 }
 
-{
+//Functionality of the transition button
 
-    //this looks for the functionality of the button
-
-    document.getElementById ('transition').addEventListener ('click', (e)=>{
-        window.location.href = "/Frontend/HTML/login.html"
-    })
-}
+document.getElementById("transition").addEventListener("click", (e) => {
+  window.location.href = "/Frontend/HTML/login.html";
+});
